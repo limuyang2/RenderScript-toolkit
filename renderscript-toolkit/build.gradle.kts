5cdcd0941ea7811d9ed5fe2151b16f68dd9b9d46
limuyang <limuyang2@hotmail.com>
2025-08-27 11:21:21 +0800

    build: 更新配置

--- renderscript-toolkit/build.gradle.kts
+++ renderscript-toolkit/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        jvmToolchain(17)
     }
     externalNativeBuild {
         cmake {
